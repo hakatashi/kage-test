06fb3497f93357dc848c148b44d3effc7abb5d16
hakatashi <hakatasiloving@gmail.com>
2014-10-15 18:18:40 +0900

    Add XML optimizer

--- kage-test.js
+++ kage-test.js
@@ -1,21 +1,37 @@
 var fs = require('fs');
 var paper = require('paper');
+var xml2js = require('xml2js');
 
 paper.setup(new paper.Canvas(800, 800));
 
-fs.readFile('01.svg', {encoding: 'utf-8'}, function (error, data) {
-	paper.project.importSVG(data);
-	var layer = paper.project.activeLayer;
-	var rootObject = layer.firstChild;
-	rootObject.children.forEach(function (child) {
-		if (child.name === 'shape') {
-			child.segments.forEach(function (segment) {
-				console.log(segment.toString());
-			})
-		}
-		layer.addChild(child.clone());
-	});
-	rootObject.remove();
+var parser = new xml2js.Parser({});
+var builder = new xml2js.Builder({headless: true});
 
-	console.log(paper.project.exportSVG().outerHTML);
+function done(error) {
+	if (error) console.error(error);
+}
+
+fs.readFile('01.svg', {encoding: 'utf-8'}, function (error, data) {
+	if (error) return done(error);
+
+	parser.parseString(data.toString(), function (error, result) {
+		if (error) return done(error);
+
+		var svg = builder.buildObject(result);
+
+		paper.project.importSVG(svg);
+		var layer = paper.project.activeLayer;
+		var rootObject = layer.firstChild;
+		rootObject.children.forEach(function (child) {
+			if (child.name === 'shape') {
+				child.segments.forEach(function (segment) {
+					console.log(segment.toString());
+				});
+			}
+			layer.addChild(child.clone());
+		});
+		rootObject.remove();
+
+		console.log(paper.project.exportSVG().outerHTML);
+	});
 });
